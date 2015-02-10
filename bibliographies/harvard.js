3246139adb23f5408c4447f1053fa037323b2fb6
Michael McMillan <email@michaelmcmillan.net>
2015-02-10 09:11:35 +0100

    Refactored out formatting rules to a 'static' object.

--- bibliographies/harvard.js
+++ bibliographies/harvard.js
@@ -1,46 +1,45 @@
-function Harvard (books) {
-    
-    var books = books;
+var Book    = require('../book.js');
+var Website = require('../website.js'); 
+var Format  = require('./harvardFormatting.js');
 
+function Harvard (references) {
+    
+    var references = references;
+
+    this.getBooks = function () {
+        return references; 
+    }
+    
     this.sort = function () {
-        books.sort(function (bookOne, bookTwo) {
-            var firstSurname  = bookOne.getAuthors()[0].getName().split(' ').reverse()[0];
-            var secondSurname = bookTwo.getAuthors()[0].getName().split(' ').reverse()[0];
+        references.sort(function (referenceOne, referenceTwo) {
+            var firstSurname  = referenceOne.getAuthors()[0].getName().split(' ').reverse()[0];
+            var secondSurname = referenceTwo.getAuthors()[0].getName().split(' ').reverse()[0];
             return firstSurname.localeCompare(secondSurname);  
         }); 
     }
-
-    this.getBooks = function () {
-        return books; 
-    }
     
-    this.authorFormat = function (bookIndex) {
-        var authorLine = '';
+    this.authorFormat = function (referenceIndex) {
+        if (references[referenceIndex] instanceof Book) 
+            return Format.book.author(references[referenceIndex].getAuthors());
         
-        books[bookIndex].getAuthors().forEach(function (author, index) {
-            authorLine += author.getSurname() + ', ' + 
-                          author.getInitials(author.getForename())
-                          .split('').join('.') + '.';
-
-            if (index == books[bookIndex].getAuthors().length - 2)
-                authorLine += ' og ';
-            else if (index < books[bookIndex].getAuthors().length - 2)
-                authorLine += ', ';
-        });
-       
-        return authorLine.trim();
+        if (references[referenceIndex] instanceof Website) 
+            console.log ('Not yet supported.');
     }
     
-    this.yearFormat = function (bookIndex) {
-        return '(' + books[bookIndex].getPublicationYear() + ')';
+    this.yearFormat = function (referenceIndex) {
+        if (references[referenceIndex] instanceof Book) 
+            return Format.book.publicationYear(references[referenceIndex].getPublicationYear());
+ 
+        if (references[referenceIndex] instanceof Website) 
+            console.log ('Not yet supported.');
     }
     
-    this.titleFormat = function (bookIndex) {
-        return '<em>' + books[bookIndex].getTitle() + '</em>'; 
+    this.titleFormat = function (referenceIndex) {
+        return '<em>' + references[referenceIndex].getTitle() + '</em>'; 
     }
     
-    this.placeFormat = function (bookIndex) {
-        return books[bookIndex].getPublicationPlace() + ':';
+    this.placeFormat = function (referenceIndex) {
+        return references[referenceIndex].getPublicationPlace() + ':';
     }
 
     this.bookToString = function (index) {
---
+++ bibliographies/harvardFormatting.js
@@ -0,0 +1,25 @@
+HarvardFormatting = {
+    
+    book: {
+        author: function (authors) {
+            var authorLine = '';
+            authors.forEach(function (author, index) { 
+                authorLine += author.getSurname() + ', ' + 
+                    author.getInitials(author.getForename())
+                    .split('').join('.') + '.';
+                
+                if (index == authors.length - 2)
+                    authorLine += ' og ';
+                else if (index <  authors.length - 2)
+                    authorLine += ', ';
+            });
+            return authorLine;
+        },
+
+        publicationYear: function (year) {
+            return '(' + year + ')';                 
+        }
+    }
+};
+
+module.exports = HarvardFormatting;
