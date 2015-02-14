b5f38847df982b785b3e334af7a73d74c3fdb000
Michael McMillan <email@michaelmcmillan.net>
2015-02-14 17:37:04 +0100

    Added author-field to Website-object.

--- website.js
+++ website.js
@@ -1,14 +1,23 @@
 var validator = require('url');
 
 function Website (url, title) {
-    var url   = url;
-    var title = title; 
+    var url     = url;
+    var title   = title; 
+    var authors = [];
 
     this.getHostname = function () {
         var hostname = validator.parse(url).hostname;
         return hostname.charAt(0).toUpperCase() + 
                hostname.slice(1)
                .substring(0, hostname.lastIndexOf('.') - 1);
+    }
+    
+    this.addAuthor = function (author) {
+        authors.push(author);
+    }
+
+    this.getAuthors = function () {
+        return authors;
     }
 
     this.setTitle = function (websiteTitle) {
