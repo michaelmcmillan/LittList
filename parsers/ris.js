dbf84db42ae95a6aae606bdce1a45a7fcde1e5a8
Michael McMillan <email@michaelmcmillan.net>
2015-02-20 18:10:10 +0100

    Improved regex to look for \s{2}, validation for start-and-end fields.

--- parsers/ris.js
+++ parsers/ris.js
@@ -2,19 +2,37 @@ function RisParser (source) {
     
     var risFields  = require('./risFields.js');
     var risTypes   = require('./risTypes.js');
-    var risPattern = /^([0-9|A-Z]{2})  \- (.*)/;
+    var risPattern = /^([A-Z]{2})\s{2}\-\s?(.*)/;
     
     var matches    = [];
     var fields     = {};
 
-    var validate = function () {
+    var validRisFormat = function () {
         
+        var fieldKeys = Object.keys(fields);
+       
         if (matches.length === undefined)
             throw new Exception ('No RIS fields found.');
         
-        for (field in fields)
+        for (field in fields) {
+            
             if (risFields[field] === undefined)
                 throw new Exception ('Illegal RIS field.');
+            
+            if (fields[field] === undefined && field !== 'ER')
+                throw new Exception ('Missing value for field.');
+
+            if (field == 'TY' && risTypes[fields[field]] === undefined) 
+                throw new Exception ('Illegal reference type.');
+        }
+        
+        if (fieldKeys[0] !== 'TY')
+            throw new Exception ('First field is not TY (type of reference).');
+        
+        if (fieldKeys[fieldKeys.length - 1] !== 'ER')
+            throw new Exception ('Last field is not ER (end reference).');
+        
+        return true;
     }
 
     this.parse = function () {
@@ -26,9 +44,8 @@ function RisParser (source) {
         for (i = 0; i < matches.length; i++)
             fields[matches[i][1]] = matches[i][2];
         
-        validate();
-        
-        return fields;
+        if (validRisFormat())
+            return fields;
     }
 }
 
