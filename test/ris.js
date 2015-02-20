7008dc786ed08f3f201fcf5b1f70a4e5633fcdd5
Michael McMillan <email@michaelmcmillan.net>
2015-02-20 18:35:23 +0100

    Removed dead test code.

--- test/ris.js
+++ test/ris.js
@@ -3,31 +3,6 @@ var RisParser = require('../parsers/ris.js');
 
 describe('ris', function () {
 
-    var risSource;
-
-    before(function () {
-        risSource = [
-            'TY  - BOOK',
-            'ID  - 101342446',
-            'T1  - Det tenkende mennesket: filosofi- og vitenskapshistorie med vitenskapsteori',
-            'A1  - Dybvig, Dagfinn Døhl 1972-',
-            'A1  - Dybvig, Magne 1940-',
-            'Y1  - 2003///',
-            'N1  - 1. utg. 2000',
-            'N2  - 4. oppl. 2009 med 13-sifret ISBN',
-            'IS  - 2. utg.',
-            'SP  - 520 s. : ill.',
-            'PB  - Tapir akademisk forl.',
-            'CY  - Trondheim',
-            'KW  - Science',
-            'KW  - Philosophy',
-            'KW  - Historisk framstilling',
-            'KW  - Vitenskapsfilosofi',
-            'KW  - Filosofi',
-            'ER  - '
-        ].join('\n'); 
-    });
-
     it('should throw an exception on a ris-file missing fields', function () {
         var ris = new RisParser('this does not look like ris format'); 
         assert.throws(function () {
