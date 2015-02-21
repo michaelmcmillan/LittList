6e0a8f18cf30fbfaefa27cf4fdbf846f71d34747
Michael McMillan <email@michaelmcmillan.net>
2015-02-21 11:59:21 +0100

    Count occurences of TY/ER to make sure its even.

--- parsers/ris/ris.js
+++ parsers/ris/ris.js
@@ -6,10 +6,9 @@ function RisParser (source) {
     
     var matches    = [];
     var fields     = {};
+    var fieldKeys  = []; 
 
     var validRisFormat = function () {
-        
-        var fieldKeys = Object.keys(fields);
        
         for (field in fields) {
             
@@ -28,10 +27,10 @@ function RisParser (source) {
         
         if (fieldKeys[fieldKeys.length - 1] !== 'ER')
             throw new Error ('Last field is not ER (end reference).');
-        
+
         return true;
     }
-
+   
     this.parse = function () {
         
         source.split('\n').forEach(function (line) {
@@ -44,9 +43,19 @@ function RisParser (source) {
         if (matches[0] === null)
             throw new Error ('No fields found.');
         
-        for (i = 0; i < matches.length; i++)
+        var TY = 0, ER = 0;
+        for (i = 0; i < matches.length; i++) {
+            if (matches[i][1] === 'TY') TY += 1; 
+            if (matches[i][1] === 'ER') ER += 1; 
+             
             fields[matches[i][1]] = matches[i][2];
+        }
+        
+        if (TY - ER !== 0)
+            throw new Error ('Uneven occurences of TY/ER.');
 
+        fieldKeys = Object.keys(fields);
+        
         if (validRisFormat())
             return fields;
     }
