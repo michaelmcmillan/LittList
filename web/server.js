7864a0b7cb1632d918c13f55a0b112c1ee4da1b6
Michael McMillan <email@michaelmcmillan.net>
2015-02-21 12:00:28 +0100

    Implemented a simple view with a form for the webserver.

--- web/server.js
+++ web/server.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var app     = express();
 
+app.use(express.static(__dirname + '/assets'));
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+
 app.get('/', function (request, response) {
-    response.send('Hello world.');
+    response.render('index.html'); 
 });
 
-app.listen(3000);
+app.post('/book', function () {
+
+});
+
+app.post('/website', function () {
+
+});
+
+app.listen(8000);
