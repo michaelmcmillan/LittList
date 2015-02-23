de75413c1797f4902fead76955a9315b9e16c7a3
Michael McMillan <email@michaelmcmillan.net>
2015-02-24 00:31:44 +0100

    Webserver route for search and config created.

--- web/server.js
+++ web/server.js
@@ -1,20 +1,26 @@
 var express = require('express');
+var config  = require('../config.js');
+var port    = config.web.port;
 var app     = express();
 
 app.use(express.static(__dirname + '/assets'));
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 
-app.get('/', function (request, response) {
+app.get('/', function (req, res) {
     response.render('index.html'); 
 });
 
-app.post('/book', function () {
-
+app.get('/search/:query', function (req, res) {
+    var query = req.params.query;
+    res.json({
+        books: [
+            {}, {}, {}
+        ]
+    });
 });
 
-app.post('/website', function () {
-
+app.listen(port, function () {
+    console.log('[*] LittList listening on port', port);
 });
 
-app.listen(8000);
