4fdc545f4a2274876e7f34d31d7acf71610146b8
Michael McMillan <email@michaelmcmillan.net>
2015-02-27 10:27:42 +0100

    Removed curl-scraper.sh poc

--- parsers/bibsys/curl-scraper.sh
+++
@@ -1,4 +0,0 @@
-# Unfortunatly bibsys uses the Java Session to determine what
-# document to export, this means we need to send two http reqs
-# per export.
-curl --data "eksportFormat=refmanager&cmd=sendtil" --cookie "JSESSIONID=CEB3DB638C41D6C4A7C68FE52296457E" http://ask.bibsysk/action/show -L | iconv -f iso8859-1 -t utf-8
