27e1503e646fa853617ee661c0fd1ce2ac914680
Michael McMillan <email@michaelmcmillan.net>
2015-03-02 19:30:57 +0100

    Added a dummy config to pass Travis tests.

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    web: {
+        port: 8000     
+    },
+
+    database: {
+        dialect:  'mysql',
+        database: '',
+        username: '',
+        password: '',
+        host:     '',
+    }
+}
