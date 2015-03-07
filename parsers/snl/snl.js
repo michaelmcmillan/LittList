1cdc7bf065a2d1f5dcfb76d14eaba75260d0dd02
Michael McMillan <email@michaelmcmillan.net>
2015-03-07 14:02:26 +0100

    Implemented parser for SNL (store norske leksikon) articles.

--- parsers/snl/snl.js
+++ parsers/snl/snl.js
@@ -1,1 +1,62 @@
- 
+var request = require('request');
+
+function SNL () {
+
+    var self = this; 
+    var urlFilter  = 'snl.no/';
+    var reqOptions = {
+        json: true,
+        headers: {
+            'User-Agent': 'LittList.no'
+        }
+    }; 
+    var protocol   = 'https://';
+    var host       = 'snl.no';
+    
+    this.getAsReference = function (url) {
+        var pageTitle = this.getArticleFromURL(url);
+        var apiURL    = protocol + host + '/' + pageTitle + '.json'; 
+
+        reqOptions.url = apiURL; 
+        request.get(reqOptions, function (err, data) {
+            if (err) throw err;
+
+            var meta = {
+                title: '',
+                pubDate: '',
+                authors: []
+            };
+            
+            meta.title = data.body.title;
+            meta.pubDate = data.body.changed_at;
+            data.body.authors.forEach(function (author) {
+                meta.authors.push(author['full_name']);
+            });
+
+            console.log(meta);
+        });
+    }
+    
+    this.isSNLURL = function (url) {
+        return !!url.indexOf(urlFilter)
+        && (url.substring(url.indexOf(urlFilter))
+        .match(/\//g)||[]).length === 1;
+    }
+
+    this.getArticleFromURL = function (url) {
+        url = this.stripHashbang(url);
+        return url.substring(url.indexOf(urlFilter) + urlFilter.length);
+    }
+
+    this.stripHashbang = function (url) {
+        if (url.indexOf('#') !== -1)
+            return url.substring(0, url.indexOf('#'));
+        else
+            return url;
+    }
+}
+
+var snl = new SNL();
+snl.getAsReference('https://snl.no/Jens_Stoltenberg');
+
+module.exports = SNL;
---
+++ test/snl.js
@@ -0,0 +1,25 @@
+var assert = require('assert');
+var SNL    = require('../parsers/snl/snl.js');
+
+describe('Store norske leksikon', function () {
+    
+    it('should pass if url is from snl.no', function () {
+        var snl = new SNL();  
+        assert.equal(snl.isSNLURL('http://snl.no/Jens_Stoltenberg'), true);
+    });
+    
+    it('should pass if url contains only one forward slash', function () {
+        var snl = new SNL();  
+        assert.equal(snl.isSNLURL('http://snl.no/om-oss/ansatte'), false);
+    });
+
+    it('should return the article name from the url', function () {
+        var snl = new SNL();
+        assert.equal(snl.getArticleFromURL('http://snl.no/Jens_Stoltenberg'), 'Jens_Stoltenberg');
+    });
+    
+    it('should avoid hashbangs in the url', function () {
+        var snl = new SNL();
+        assert.equal(snl.getArticleFromURL('http://snl.no/Erna_Solberg#Karriere'), 'Erna_Solberg');
+    });
+});
