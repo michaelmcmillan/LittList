cf5575ae280c45e22116382bb06aae1f42cdfb35
Michael McMillan <email@michaelmcmillan.net>
2015-03-08 17:18:25 +0100

    Overhauled design and controller now responds to queries (it's alive muuhaha).

--- web/server.js
+++ web/server.js
@@ -4,20 +4,27 @@ var config     = require('../config.js');
 var port       = config.web.port;
 var app        = express();
 
+var Bibsys     = require('../parsers/bibsys/bibsys.js');
+
 app.use(express.static(__dirname + '/assets'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'handlebars');
 app.engine('handlebars', handlebars({
-    // This is a horrible hack due to shortcomings
-    // of express-handlebars dir handling.
     defaultLayout: '../../web/views/layouts/main'
 }));
 
 app.get('/', function (req, res) {
+
     if (req.query.q) {
-        res.render('results', {
-            query: req.query.q
-        }); 
+        console.log(req.query.q);
+
+        var bibsys = new Bibsys();
+        bibsys.search(req.query.q, function (results) {
+            res.render('results', {
+                query: req.query.q,
+                results: results
+            }); 
+        });
     } else {
         res.render('index'); 
     }
@@ -30,4 +37,3 @@ app.get('/liste/:id', function (req, res) {
 app.listen(port, function () {
     console.log('[*] LittList listening on port', port);
 });
-
