bbb4d5a8e0ec5263c49aaed166493ba91b336d50
Michael McMillan <email@michaelmcmillan.net>
2015-03-08 18:56:31 +0100

    Tips caroussel implemented.

---
+++ web/assets/js/tips.js
@@ -0,0 +1,33 @@
+(function () { 
+    var self   = this;
+    var tips   = document.querySelectorAll('.tips > .tip');
+    var btns   = document.querySelectorAll('.tips > span');
+    this.index = 0;
+
+    var rotateTips = function (index) {
+        var current = index % (tips.length);
+        
+        tips[current].style.display = 'block';
+        btns[current].className = 'active';
+
+        for (i = 0; i < btns.length; i++)
+            if (i !== current) {
+                btns[i].className = '';
+                tips[i].style.display = 'none';
+            }
+    }
+
+    for (i = 0; i < btns.length; i++) {
+        btns[i].addEventListener('click', function (event) {
+            var element = event.srcElement;
+            var sibling = element.parentNode.children;
+            self.index = Array.prototype.indexOf.call(sibling, element) - 3;
+            rotateTips(self.index);
+        });
+    }    
+
+    rotateTips(this.index);
+    setInterval(function () {
+        rotateTips(self.index++);
+    }, 7500);
+}());
