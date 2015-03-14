165d33935d225827a3f58b298fb02ff279cd0833
Michael McMillan <email@michaelmcmillan.net>
2015-03-14 14:52:01 +0100

    Removed comments and debug logging and log if no results found by query.

--- web/controllers/index.js
+++ web/controllers/index.js
@@ -2,17 +2,10 @@ var log = require('../../log/logger.js');
 var QueryController = require('./query.js');
 
 function IndexController (req, res) {
-
-    log.error('Test');
-    
-    // Delegate to QueryController if a search is present
     if (req.query.q)
         QueryController(req, res);
-    
-    // Show the landing page
     else 
         res.render('index'); 
-
 }
 
 module.exports = IndexController;
--- web/controllers/query.js
+++ web/controllers/query.js
@@ -1,13 +1,20 @@
+var logger = require('../../log/logger.js');
 var Bibsys = require('../../parsers/bibsys/bibsys.js');
 
 function QueryController (req, res) {
     var bibsys = new Bibsys();
+    var query  = req.query.q;
 
-    bibsys.search(req.query.q, function (results) {
-        res.render('results', {
-            query: req.query.q,
-            results: results
-        }); 
+    bibsys.search(query, function (results) {
+        if (results.length !== 0) {
+            res.render('results', {
+                query: query,
+                results: results
+            });
+        } else {
+            logger.info('No results found for "' + query + '"');
+            res.render('results');
+        }
     });
 }
 
