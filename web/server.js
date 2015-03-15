8c59cf081b380b6160e847cda20f6c1df4e012bb
Michael McMillan <email@michaelmcmillan.net>
2015-03-15 23:46:38 +0100

    Commented and included express-session in server.js

--- web/server.js
+++ web/server.js
@@ -1,23 +1,32 @@
 /* Dependencies */
 var express     = require('express');
+var session     = require('express-session');
 var handlebars  = require('express-handlebars'); 
+var app         = express();
+
+/* Application */
 var config      = require('../config.js');
 var routes      = require('./routes.js'); 
 var logger      = require('../log/logger.js');
-var port        = config.web.port;
-var app         = express();
 
 /* Bootstrap */
 app.use(express.static(__dirname + '/assets'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'handlebars');
 app.use('/', routes);
+
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: 'e'
+}));
+
 app.engine('handlebars', handlebars({
     defaultLayout: __dirname + '/views/layouts/main',
     partialsDir:   __dirname + '/views/partials/'
 }));
 
 /* Lift-off */
-app.listen(port, function () {
-    logger.info('LittList is now listening on port', port);
+app.listen(config.web.port, function () {
+    logger.info('LittList is now listening on port', config.web.port);
 });
