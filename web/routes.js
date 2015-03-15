3a7223cdc0a399613a46c9c2f74e2b3112e87b0a
Michael McMillan <email@michaelmcmillan.net>
2015-03-15 23:46:11 +0100

    Added routes for /liste and liste/:id

--- web/routes.js
+++ web/routes.js
@@ -3,6 +3,9 @@ var router = require('express').Router();
 
 module.exports = (function () {
     router.get('/', controllers.index);
+    router.get('/liste', controllers.list);
+    router.post('/liste', controllers.list);
     router.get('/liste/:id', controllers.list);
+    router.post('/liste/:id', controllers.list);
     return router;
 })();
