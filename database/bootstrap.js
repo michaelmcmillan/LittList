0ae825fb71e92c3869bec339ab236f00999b9108
Michael McMillan <email@michaelmcmillan.net>
2015-03-17 23:07:19 +0100

    Use winston for outputting queries.

--- database/bootstrap.js
+++ database/bootstrap.js
@@ -1,11 +1,12 @@
 var config = require('../config.js');
 var Sequelize = require('sequelize');
+var logger = require('../log/logger.js');
 
 module.exports = new Sequelize(
     config.database.database,
     config.database.username,
     config.database.password, {
-        
+        logging: logger.debug,        
         host:    config.database.host,
         dialect: config.database.dialect,
         pool: {
