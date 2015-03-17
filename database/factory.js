cbb16e5c69f20d0256e018820e15ae1dc4d7954e
Michael McMillan <email@michaelmcmillan.net>
2015-03-17 23:07:59 +0100

    Renamed bookModel, properly implemented save and get for a single book-object.

--- database/factory.js
+++ database/factory.js
@@ -1,17 +1,29 @@
-var bookModel    = require('./schemas/book.js');
-var websiteModel = require('./schemas/website.js');
+var bookSchema    = require('./schemas/book.js');
+var websiteSchema = require('./schemas/website.js');
 
 function Factory () {
     
     this.book = {
-        get: function (id) {
-            return bookModel.find(id);
+        get: function (id, cb) {
+            return bookSchema.find(id).then(function (book) {
+                cb(book.get({plain: true})); 
+            });
+        },
+
+        save: function (book) {
+            bookSchema.create({
+                isbn: book.getISBN(),
+                title: book.getTitle(),
+                author: book.getAuthors(),
+                publisher: book.getPublisher()[0],
+                publicationYear: book.getPublicationYear() 
+            });
         }
     };
 
     this.website = {
         get: function (id) {
-            return websiteModel.find(id);
+            return websiteSchema.find(id);
         }
     };
 }
