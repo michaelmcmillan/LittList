94355e92140a5f21d0459328a237e2594ad3782a
Michael McMillan <email@michaelmcmillan.net>
2015-03-18 21:35:03 +0100

    POST /liste on add btn from results.

--- web/assets/js/results.js
+++ web/assets/js/results.js
@@ -1,12 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
     var entries = document.querySelectorAll('.entry');
+    var input   = document.createElement('input');
+    var form    = document.querySelector('form#list');
+    input.type  = 'hidden';
+    input.name  = 'book';
+    form.appendChild(input);
 
     for (i = 0; i < entries.length; i++) {
-        var entry = entries[i];
-        var add   = entry.querySelector('.controls > .add');
-
-        add.addEventListener('click', function () {
-            alert('hehe');
+        var add = entries[i].querySelector('.controls > .add');
+        add.addEventListener('click', function (event) {
+             var entry = event.srcElement.parentNode.parentNode;
+             input.value = entry.getAttribute('data-id');
+             form.submit();
         });
     }
 });
