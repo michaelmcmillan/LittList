6920dbc19449427566ec4a45a9ec828d4ee6ca06
Michael McMillan <email@michaelmcmillan.net>
2015-03-18 20:17:30 +0100

    Passes the searchbar test.

--- test/integration/searchbar.js
+++ test/integration/searchbar.js
@@ -1,12 +1,11 @@
-var casper = require('casper').create();
 var port   = require('../../config.js').web.port;
+var domain = 'http://localhost:' + port;
 
-casper.start('http://localhost:' + port, function() {
-    this.echo(this.getTitle());
+casper.test.begin('Searching for a popular book returns more than 1 result', 1, function suite(test) {
+    casper.start(domain, function () {
+        test.assertExists('form > input[name="q"]', "found search form");
+    });
 });
 
-casper.thenOpen('http://phantomjs.org', function() {
-    this.echo(this.getTitle());
-});
 
 casper.run();
