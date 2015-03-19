fbf56a5ef1e152fe5df72525f476b7f20d8f16ed
Michael McMillan <email@michaelmcmillan.net>
2015-03-19 18:27:59 +0100

    Improved test for searchbar in casperjs.

--- test/integration/searchbar.js
+++ test/integration/searchbar.js
@@ -1,11 +1,23 @@
 var port   = require('../../config.js').web.port;
 var domain = 'http://localhost:' + port;
 
-casper.test.begin('Searching for a popular book returns more than 1 result', 1, function suite(test) {
+casper.test.begin('Searching for a popular book returns more than 1 result', 2, function suite(test) {
+
     casper.start(domain, function () {
-        test.assertExists('form > input[name="q"]', "found search form");
+        this.fill('form#query', {
+            q: "ingvar ambjørnsen"
+        }, true);
+    });
+
+    casper.then(function () {
+        test.assertUrlMatch(/q=ingvar/, 'reached the results page');
+    });
+    
+    casper.then(function () {
+        test.assertExist('.entry');
+    });
+
+    casper.run(function() {
+        test.done();
     });
 });
-
-
-casper.run();
