7c24f698de75356826446c11fd83d4deeca220d4
Michael McMillan <email@michaelmcmillan.net>
2015-03-20 15:57:48 +0100

    Removed old connection setup

--- database/bootstrap.js
+++ database/bootstrap.js
@@ -7,13 +7,6 @@ if (process.env['TRAVIS'] !== undefined)
 else
     var connection = mysql.createConnection(config.database);
 
-var connection = mysql.createConnection({
-    host: config.database.host,
-    user: config.database.username,
-    database: config.database.database,
-    password: config.database.password
-});
-
 connection.connect();
 
 module.exports = connection;
