382037cc531e4a0513982479da52d1440375ebc7
Michael McMillan <email@michaelmcmillan.net>
2015-03-20 15:08:22 +0100

    authorFactory implemented

---
+++ test/database/authorFactory.js
@@ -0,0 +1,22 @@
+var assert      = require('assert');
+var mysql       = require('mysql');
+var config      = require('../../config.js');
+var Author      = require('../../models/author.js');
+var AuthorFactory = require('../../database/factories/author.js');
+
+describe('authorFactory', function () {
+    it('changes database to the testdatabase', function (done) {
+        AuthorFactory.database.changeUser({
+            database: config.database.test_database
+        }, function (err) {
+            done();
+        });
+    });
+
+    it('creates an author entry in the database for reference 1', function (done) {
+        var author = new Author('Jo Nesbø');
+        AuthorFactory.create(1, author, function (results) {
+            done();
+        });
+    });
+}); 
