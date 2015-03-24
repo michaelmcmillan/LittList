642b1eea88d9dd433ede8a95aab195807b448a3c
Michael McMillan <email@michaelmcmillan.net>
2015-03-24 01:56:28 +0100

    removed Promise invocation which broke node 0.10 tests

--- database/factories/reference.js
+++ database/factories/reference.js
@@ -1,9 +1,5 @@
 var database = require('../bootstrap.js');
 
-var p1 = new Promise(function(resolve, reject) {
-  resolve("Success!");
-});
-
 var ReferenceFactory = {
     create: function (title, cb) {
         database.query('INSERT INTO `References` SET ?', {
