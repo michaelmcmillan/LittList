886e5af0c6608b402149d9bb155d9c0c558012e0
Michael McMillan <email@michaelmcmillan.net>
2015-03-24 01:17:55 +0100

    Moved creation of database into own script in npm. Simply too much hassle with node-mysql.

--- database/bootstrap.js
+++ database/bootstrap.js
@@ -1,21 +1,9 @@
 var config = require('../config.js');
 var logger = require('../log/logger.js');
 var mysql  = require('mysql');
+if (process.env['TRAVIS'] === undefined)
+    var pool = mysql.createPool(config.database);
+else 
+    var pool = mysql.createPool(config.database.test);
 
-if (process.env['TRAVIS'] !== undefined) {
-    var connection = mysql.createConnection({
-        host: config.database.test.host,
-        user: config.database.test.user,
-        password: config.database.test.password
-    });
-} else {
-    var connection = mysql.createConnection({
-        host: config.database.host,
-        user: config.database.user,
-        password: config.database.password,
-        database: config.database.database
-    });
-}
-
-connection.connect();
-module.exports = connection;
+module.exports = pool;
