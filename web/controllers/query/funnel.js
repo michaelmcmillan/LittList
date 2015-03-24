b8437d331478680519fce6f6194d48accb7d75d1
Michael McMillan <email@michaelmcmillan.net>
2015-03-24 02:10:53 +0100

    do cache lookup for debugging

--- web/controllers/query/funnel.js
+++ web/controllers/query/funnel.js
@@ -1,22 +1,26 @@
+var QueryFactory        = require('../../../database/factories/query.js'); 
 var BibsysController    = require('./bibsys.js');
 var Bibsys              = require('../../../parsers/bibsys/bibsys.js');
-
 var WikipediaController = require('./wikipedia.js');
 var Wikipedia           = require('../../../parsers/wikipedia/wikipedia.js');
-
 var SNLController       = require('./snl.js');
 var SNL                 = require('../../../parsers/snl/snl.js');
 
 function FunnelController (req, res) {
+
     var wikipedia = new Wikipedia()
     var bibsys    = new Bibsys()
     var snl       = new SNL();
+    
+    QueryFactory.read(req.query.q, function (row) {
+        console.log(row);
+    });
 
     if (wikipedia.isWikipediaURL(req.query.q))
         WikipediaController(req, res);
 
     else if (snl.isSNLURL(req.query.q))
-        SNLController(req, res);    
+        SNLController(req, res);
 
     else
         BibsysController(req, res);
