1a92619bb336bff67b1769ea4a26a75a609fd003
Michael McMillan <email@michaelmcmillan.net>
2015-03-28 11:16:35 +0100

    simplified test db connection

--- database/bootstrap.js
+++ database/bootstrap.js
@@ -1,9 +1,8 @@
 var config = require('../config.js');
 var logger = require('../log/logger.js');
 var mysql  = require('mysql');
-if (process.env['TRAVIS'] === undefined)
-    var pool = mysql.createPool(config.database);
-else 
-    var pool = mysql.createPool(config.database.test);
 
-module.exports = pool;
+if (process.env['TRAVIS'] === undefined)
+    config.database = config.database.test;
+
+module.exports = mysql.createPool(config.database);
