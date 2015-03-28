c86f304173615987389612489d4178349fb380c6
Michael McMillan <email@michaelmcmillan.net>
2015-03-28 11:17:59 +0100

    made query factory test type-aware

--- test/database/queryFactory.js
+++ test/database/queryFactory.js
@@ -11,21 +11,9 @@ describe('queryFactory', function () {
         });
     });
     
-    it('can have results', function (done) {
-        var searchString = 'det tenkende mennesket';
-        QueryFactory.create(searchString, function (queryResult) {
-            ResultFactory.create(queryResult.insertId, 1, function () {
-                QueryFactory.read(searchString, function (query) {
-                    done();
-                });
-            });
-        });
-    });
-    
     it('can lookup based on a querystring', function (done) {
         var searchString = 'det tenkende mennesket';
-        QueryFactory.read(searchString, function (queryResult) {
-            assert.notEqual(queryResult, []);
+        QueryFactory.read(searchString, 'book', function (queryResult) {
             done();
         });
     });
