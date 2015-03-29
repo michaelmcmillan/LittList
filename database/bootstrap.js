61525af507bf99b565e66b872efefd8ce27d8c92
Michael McMillan <email@michaelmcmillan.net>
2015-03-30 01:01:01 +0200

    wrong logic check for testing

--- database/bootstrap.js
+++ database/bootstrap.js
@@ -2,7 +2,7 @@ var config = require('../config.js');
 var logger = require('../log/logger.js');
 var mysql  = require('mysql');
 
-if (process.env['TRAVIS'] === undefined)
+if (process.env['TRAVIS'] !== undefined)
     config.database = config.database.test;
 
 module.exports = mysql.createPool(config.database);
