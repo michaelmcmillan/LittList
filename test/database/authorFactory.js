67e681b23bff430f8212b89f4e7c8bad28d17fa1
Michael McMillan <email@michaelmcmillan.net>
2015-03-30 13:50:17 +0200

    removed unused authorfactory test

--- test/database/authorFactory.js
+++
@@ -1,8 +0,0 @@
-var assert      = require('assert');
-var mysql       = require('mysql');
-var config      = require('../../config.js');
-var Author      = require('../../models/author.js');
-var AuthorFactory = require('../../database/factories/author.js');
-
-describe('authorFactory', function () {
-}); 
