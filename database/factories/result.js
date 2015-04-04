99f8379eb1e70a63f2b61f76b5b9728737fd1c58
Michael McMillan <email@michaelmcmillan.net>
2015-04-04 16:36:19 +0200

    better err handling in create-method

--- database/factories/result.js
+++ database/factories/result.js
@@ -2,13 +2,13 @@ var database = require('../bootstrap.js');
 
 var ResultFactory = {
 
-    create: function (query_id, reference_id, cb) {
+    create: function (query_id, reference_id, done) {
         database.query('INSERT INTO Results SET ?', {
             query_id: query_id,
             reference_id: reference_id
         }, function (err, result) {
-            if (err) throw err;
-            cb(result);
+            if (err) return done(err);
+            done(undefined, result);
         });
     }
 }
