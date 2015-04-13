00c48dcf02d331c36cd2f4eb171cc57da9ecc46a
Michael McMillan <email@michaelmcmillan.net>
2015-04-13 13:46:53 +0200

    js for results no longer needed

--- web/assets/js/results.js
+++ web/assets/js/results.js
@@ -1,17 +1,3 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var entries = document.querySelectorAll('.entry');
-    var input   = document.createElement('input');
-    var form    = document.querySelector('form#list');
-    input.type  = 'hidden';
-    input.name  = 'book';
-    form.appendChild(input);
 
-    for (i = 0; i < entries.length; i++) {
-        var add = entries[i].querySelector('.controls > .add');
-        add.addEventListener('click', function (event) {
-             var entry = event.srcElement.parentNode.parentNode;
-             input.value = entry.getAttribute('data-id');
-             form.submit();
-        });
-    }
 });
