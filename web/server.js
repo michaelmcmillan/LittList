bfc029ab55918c75092e9a9d0d60baaddb592230
Michael McMillan <email@michaelmcmillan.net>
2015-04-14 00:00:14 +0200

    persistent session store

--- web/server.js
+++ web/server.js
@@ -1,14 +1,15 @@
 /* Dependencies */
-var express     = require('express');
-var session     = require('express-session');
-var handlebars  = require('express-handlebars'); 
-var bodyParser  = require('body-parser')
-var app         = express();
+var express      = require('express');
+var session      = require('express-session');
+var sessionStore = require('express-mysql-session')
+var handlebars   = require('express-handlebars'); 
+var bodyParser   = require('body-parser')
+var app          = express();
 
 /* Application */
-var config      = require('../config.js');
-var routes      = require('./routes.js'); 
-var logger      = require('../log/logger.js');
+var config       = require('../config.js');
+var routes       = require('./routes.js'); 
+var logger       = require('../log/logger.js');
 
 /* Bootstrap */
 app.use(bodyParser.urlencoded({extended: true}));
@@ -16,17 +17,22 @@ app.use(express.static(__dirname + '/assets'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'handlebars');
 
+/* Session */
 app.use(session({
-    resave: false,
-    saveUninitialized: false,
-    secret: 'e'
+    key: 'littlist',
+    secret: 'e',
+    store: new sessionStore(config.database()),
+    resave: true,
+    saveUninitialized: false
 }));
 
+/* Templating */
 app.engine('handlebars', handlebars({
     defaultLayout: __dirname + '/views/layouts/main',
     partialsDir:   __dirname + '/views/partials/'
 }));
 
+/* Routes */
 app.use('/', routes);
 
 /* Lift-off */
