a4f4b3be13980eebf0a59b5386e2ddd704189b71
Michael McMillan <email@michaelmcmillan.net>
2015-04-18 15:29:33 +0200

    indented mocks in test for search

--- test/unit/search.js
+++ test/unit/search.js
@@ -3,8 +3,8 @@ var rewire = require("rewire");
 var IndexController = rewire('../../controllers/index.js');
 
 describe('Search', function () {
-        var reqMock = { query: {}}
-        var resMock = {}
+    var reqMock = { query: {}}
+    var resMock = {}
 
     it('should render index if the search parameter (q) is not present', function () {
         resMock.render = function (view) {
