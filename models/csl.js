20fc353089281fa6734df0cdcf06f8991ad431a4
Michael McMillan <email@michaelmcmillan.net>
2015-04-22 08:47:25 +0200

    model with test for converting list with references to csl compatible data (json)

--- models/csl.js
+++ models/csl.js
@@ -1,29 +1,36 @@
 var Book = require('./book.js');
 
-function CSL (reference) {
+function CSL (list) {
     
     var items = {};
+    
+    list.getReferences().forEach(function (reference) {
+        
+        // Converts a Book model to a CSL-JSON
+        // compatible data format.
+        if (reference instanceof Book) {
+            var id = reference.getId(); 
 
-    if (reference instanceof Book) {
-        var id = reference.getId(); 
-
-        items[id]                    = {};
-        items[id].type               = 'book'; 
-        items[id].title              = reference.getTitle();
-        items[id].id                 = reference.getId();        
-        items[id].publisher          = reference.getPublisher();
-        items[id]['publisher-place'] = reference.getPublicationPlace();
-    }
-
-    if (reference.getAuthors().length > 0) {
-        items[id].author = [];
-        reference.getAuthors().forEach(function (author) {
-            items[id].author.push({
-                family: author.getSurname(),
-                given:  author.getForename()
+            items[id]                    = {};
+            items[id].id                 = reference.getId();
+            items[id].type               = 'book'; 
+            items[id].title              = reference.getTitle() || '';
+            items[id].publisher          = reference.getPublisher() || '';
+            items[id]['publisher-place'] = reference.getPublicationPlace() || '';
+        }
+    
+        // Iterates over all authors in the reference
+        // and adds them to the CSL-JSON object
+        if (reference.getAuthors().length > 0) {
+            items[id].author = [];
+            reference.getAuthors().forEach(function (author) {
+                items[id].author.push({
+                    family: author.getSurname()  || '',
+                    given:  author.getForename() || ''
+                });
             });
-        });
-    }
+        }
+    });
 
     return items; 
 }
