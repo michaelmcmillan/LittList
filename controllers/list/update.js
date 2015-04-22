b1190fab2cf1422dc414bb8fa3e38d9b9dab1eaa
Michael McMillan <email@michaelmcmillan.net>
2015-04-22 17:49:49 +0200

    documented and implemented handling of style change

--- controllers/list/update.js
+++ controllers/list/update.js
@@ -1,4 +1,5 @@
 var logger = require('../../log/logger.js');
+var config = require('../../config.js');
 var List   = require('../../models/list.js');
 var Book   = require('../../models/book.js');
 var ListFactory = require('../../database/factories/list.js');
@@ -7,24 +8,35 @@ function UpdateListController (req, res, next) {
     
     ListFactory.read(req.session.list, function (err, list) {
         if (err) return next(err);
-
+        
+        // Adding a reference
         if (req.body.add !== undefined) {
             var book = new Book().setId(parseInt(req.body.add, 10));
             list.addReference(book); 
         } 
-
+        
+        // Removing a reference
         if (req.body.remove !== undefined)
             list.removeReference(parseInt(req.body.remove, 10)); 
+        
+        // Changing the bibliography style
+        if (req.body.style !== undefined) {
+            var allowedStyleFiles = Object.keys(config.bibliography.styles.allowed);
+            if (allowedStyleFiles.indexOf(req.body.style) !== -1)
+                ; // Not implemented yet
+        }
 
+        // Update the model with the above changes to the database
         ListFactory.update(list, function (err, list) {
             if (err) return next(err);
 
             logger.log('debug', 'Updated list contents', {
-                id: list.getId(),
+                id:      list.getId(),
                 added:   req.body.add    || null,
                 removed: req.body.remove || null
             });
             
+            // Redirect to previous page if referer is available 
             if (req.headers.referer !== undefined)
                 res.redirect(req.headers.referer);
             else
