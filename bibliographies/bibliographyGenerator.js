8eea1e6253a7c20a5fdd40659207932a13794948
Michael McMillan <email@michaelmcmillan.net>
2015-04-23 17:28:08 +0200

    load the style which the list has set as its bibliography style. fix issue #18

--- bibliographies/bibliographyGenerator.js
+++ bibliographies/bibliographyGenerator.js
@@ -1,5 +1,6 @@
 var Citeproc = require('citeproc');
 var CSL      = require('../models/csl.js');
+var config   = require('../config.js');
 
 // Convert the references from the list to CiteJSON
 // format so that citeproc can intrepret it
@@ -8,12 +9,13 @@ function BibliographyGenerator (list, done) {
     // Convert the list to a CSL compatible format
     var citations = CSL(list);
 
-    // These locations should be defined in the config
-    var style  = __dirname + '/styles/chicago-fullnote-bibliography.csl';
-    var locale = __dirname + '/locales/locales-nb-NO.xml';
+    // Find the references to the style and the locale 
+    var styleFile      = list.getBibliographyStyle();
+    var styleLocation  = config.bibliography.styles.location  + styleFile;
+    var localeLocation = config.bibliography.locales.location + 'locales-nb-NO.xml';
     
     // Call the citeproc wrapper with the citations
-    Citeproc(citations, style, locale, function (citeproc) {
+    Citeproc(citations, styleLocation, localeLocation, function (citeproc) {
 
         citeproc.updateItems(Object.keys(citations));
         var bibliography = citeproc.makeBibliography();
