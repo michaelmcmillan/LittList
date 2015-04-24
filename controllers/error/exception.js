680f310455e5b9dce0b8c69d7a1146e698a301db
Michael McMillan <email@michaelmcmillan.net>
2015-04-24 08:59:43 +0200

    log if no hits were found for a querystring

--- controllers/error/exception.js
+++ controllers/error/exception.js
@@ -1,16 +1,23 @@
-function ExceptionController (error, req, res, next) {
+var logger = require('../../log/logger.js');
 
+function ExceptionController (error, req, res, next) {
+    
     if (error.message.indexOf('ingen treff') !== -1)
+        var message = 'Beklager, vi fant ingen treff for ' + 
+                req.query.q + '.';
+
         var errorMessage = {
             title: 'Ingen treff',
-            message: 'Beklager, vi fant ingen treff.'
+            message: message
         }
     
-    if (errorMessage === undefined)
+    if (errorMessage === undefined) {
+        logger.log('error', error.message);
         var errorMessage = {
             title: 'Oooops',
             message: 'En feil har oppstått. Heldigvis er en nerd på saken.'
         }
+    }
 
     res.render('error', errorMessage);
 }
