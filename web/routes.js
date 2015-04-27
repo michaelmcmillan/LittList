d6849475dca8e57f5b91230d383196b938f06d7d
Michael McMillan <email@michaelmcmillan.net>
2015-04-27 10:59:32 +0200

    removed unused else block

--- web/routes.js
+++ web/routes.js
@@ -4,15 +4,18 @@ var router = require('express').Router();
 module.exports = (function () {
 
     /* Root */
-    router.get('/', controllers.general.index);
+    router.get('/',           controllers.general.index);
 
     /* General */
-    router.get('/om', controllers.general.about);
+    router.get('/om',         controllers.general.about);
     
     /* List */
     router.get('/liste',      controllers.list.view);
     router.get('/liste/:url', controllers.list.view);
     router.post('/liste',     controllers.list.create);
+    
+    /* Posts */
+    router.get('/innlegg',    controllers.posts.view);
 
     return router;
 })();
