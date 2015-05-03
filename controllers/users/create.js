b406fe71f4da8b072d9e82de4c8a304b3adfe61e
Michael McMillan <email@michaelmcmillan.net>
2015-05-03 13:32:32 +0200

    simple controller for user creation added

---
+++ controllers/users/create.js
@@ -0,0 +1,9 @@
+var logger      = require('../../log/logger.js');
+var UserFactory = require('../../database/factories/user.js');
+
+function UserCreateController (req, res, next) {
+    
+    res.send('not implemented yet');
+}
+
+module.exports = UserCreateController;
