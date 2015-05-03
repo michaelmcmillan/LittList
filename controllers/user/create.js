3d0f0189ac5ba797dccd0b01b0bc287dff8b96ac
Michael McMillan <email@michaelmcmillan.net>
2015-05-03 15:44:30 +0200

    change session and redirect user upon successful registration

--- controllers/user/create.js
+++ controllers/user/create.js
@@ -1,9 +1,33 @@
 var logger      = require('../../log/logger.js');
+var User        = require('../../models/user.js');
 var UserFactory = require('../../database/factories/user.js');
 
 function UserCreateController (req, res, next) {
     
-    res.send('not implemented yet');
+    var email    = req.body.email;
+    var password = req.body.password;
+    
+    if (email    !== undefined
+    &&  password !== undefined) {
+        var user = new User(); 
+        user.setEmail(email);
+        user.setPassword(password, function (err) {
+            if (err) return next(err);
+            storeUserInDatabase(user);
+        });
+    }
+
+    var storeUserInDatabase = function (user) {
+        UserFactory.create(user, function (err, createdUser) {
+            if (err) return next(err);
+            authenticateAndRedirect(createdUser);
+        });
+    }
+    
+    var authenticateAndRedirect = function (user) {
+        req.session.user = user.getEmail();
+        res.redirect('/yey');
+    }
 }
 
 module.exports = UserCreateController;
