efb820a70a7560b2e5165ad2cb6be678bb1ab20f
Michael McMillan <email@michaelmcmillan.net>
2015-05-04 08:29:14 +0200

    debug log the current user

--- web/server.js
+++ web/server.js
@@ -36,6 +36,7 @@ app.engine('handlebars', handlebars({
 /* Attach ip to req */
 app.use(function (req, res, next) {
     req.ip = req.connection.remoteAddress;
+    logger.debug(req.session.user);
     next();
 });
 
