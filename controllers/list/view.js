f7713df0223bd9665519349d9cd14126d6db9c17
Michael McMillan <email@michaelmcmillan.net>
2015-06-08 14:35:55 +0200

    added logic for updating and vieweing lists with regard to locale

--- controllers/list/view.js
+++ controllers/list/view.js
@@ -1,8 +1,8 @@
 var logger      = require('../../log/logger.js');
 var config      = require('../../config.js');
-var supportedStyles = require('../../bibliographies/supportedStyles.js');
 var ListFactory = require('../../database/factories/list.js');
-var BibliographyGenerator = require('../../bibliographies/bibliographyGenerator.js');
+var BibliographyGenerator     = require('../../bibliographies/bibliographyGenerator.js');
+var supportedStylesAndLocales = require('../../bibliographies/supportedStylesAndLocales.js');
 
 function ViewListController (req, res) {
 
@@ -23,29 +23,34 @@ function ViewListController (req, res) {
                 id: list.getId()
             });
 
-            // Fetch the available reference styles
-            supportedStyles(config.bibliography.styles.allowed, config.bibliography.styles.location, 
-                function (err, styles) {
+            // Fetch the available reference styles and locales
+            supportedStylesAndLocales(function (err, styles, locales) {
                     
-                    // Mark the style used as selected 
-                    styles.forEach(function (style) {
-                        style.selected = (style.file === list.getBibliographyStyle());
-                    });
-                    
-                    // Find out if feedback form should be displayed
-                    var displayFeedback = req.query.feedback === '' ? true : false;
+                // Mark the style used as selected 
+                styles.forEach(function (style) {
+                    style.selected = (style.file === list.getBibliographyStyle());
+                });
+                
+                // Mark the locale used as selected 
+                locales.forEach(function (locale) {
+                    locale.selected = (locale.file === list.getBibliographyLocale());
+                });
+                
+                // Find out if feedback form should be displayed
+                var displayFeedback = req.query.feedback === '' ? true : false;
 
-                    // Finally render the list with references,
-                    // the generated bibliography and supported styles
-                    res.render('list', {
-                        references:            list.getReferences(),
-                        list:                  bibliography,
-                        supportedStyles:       styles,
-                        referencesInListCount: list.getReferences().length,
-                        displayFeedback:       displayFeedback,
-                        expired:               list.hasExpired(),
-                        lifetime:              list.getHumanFriendlyLifetime()
-                    }); 
+                // Finally render the list with references,
+                // the generated bibliography and supported styles
+                res.render('list', {
+                    references:            list.getReferences(),
+                    list:                  bibliography,
+                    supportedStyles:       styles,
+                    supportedLocales:      locales,
+                    referencesInListCount: list.getReferences().length,
+                    displayFeedback:       displayFeedback,
+                    expired:               list.hasExpired(),
+                    lifetime:              list.getHumanFriendlyLifetime()
+                }); 
             });
         });
     });
