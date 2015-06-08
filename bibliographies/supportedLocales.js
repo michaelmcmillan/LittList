f76f49db1fefcb7f9ce0373b28133fb50f3b91dc
Michael McMillan <email@michaelmcmillan.net>
2015-06-08 14:06:36 +0200

    renamed languages to locales

--- bibliographies/supportedLocales.js
+++ bibliographies/supportedLocales.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-function supportedLocales (allowedStyles, localesLocation, done) {
+function supportedLocales (allowedLocales, localesLocation, done) {
      
     var locales  = [];
     
