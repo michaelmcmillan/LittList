dadab97571c3cb8c543417a642c3df9171cb15a4
Michael McMillan <email@michaelmcmillan.net>
2015-07-15 11:03:14 +0200

    show error message to user if external component failed

--- controllers/error/exception.js
+++ controllers/error/exception.js
@@ -1,11 +1,17 @@
 var logger = require('../../log/logger.js');
 
 function ExceptionController (error, req, res, next) {
-    
     if (error.message.indexOf('PDF') !== -1)
         var errorMessage = {
             title: 'PDF format',
             message: 'Vi støtter dessverre ikke PDF-format enda.'
+        }
+
+    if (error.message.indexOf('finnes ikke på') !== -1)
+        var errorMessage = {
+            title: error.message,
+            message: 'Beklager, vi fant ingen treff på den eksterne siden for ' + 
+                req.query.q + '.'
         }
 
     if (error.message.indexOf('ingen treff') !== -1)
