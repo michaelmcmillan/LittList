96ab993f1e4d308067febc0e7b83ae0f9c4fa321
Michael McMillan <email@michaelmcmillan.net>
2015-12-06 23:01:06 +0100

    initial test and code for book title

---
+++ book.py
@@ -0,0 +1,28 @@
+class BookTitle(object):
+
+    def __init__(self, text = None):
+        if text is None:
+            self._text = 'Unknown title'
+        elif self.is_text_valid(text):
+            self._text = text
+
+    def is_text_valid(self, text):
+        if not text or text.isspace():
+            raise TypeError('Text can not be empty.')
+        return True
+
+    def __str__(self):
+        return self._text
+
+class Book(object):
+
+    def __init__(self):
+        self._title = BookTitle()
+
+    @property
+    def title(self):
+        return str(self._title)
+
+    @title.setter
+    def title(self, text):
+        self._title = BookTitle(text)
---
+++ tests/book_test.py
@@ -0,0 +1,18 @@
+import unittest
+from book import Book
+
+class TestBook(unittest.TestCase):
+
+    def test_default_title_is_unknown(self):
+        book = Book()
+        assert book.title == 'Unknown title'
+
+    def test_raises_error_if_title_is_only_whitespace(self):
+        with self.assertRaisesRegexp(TypeError, 'empty'):
+            book = Book()
+            book.title = ' '
+
+    def test_raises_error_if_title_is_empty(self):
+        with self.assertRaisesRegexp(TypeError, 'empty'):
+            book = Book()
+            book.title = ''
