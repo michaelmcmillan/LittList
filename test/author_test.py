a9a7f98029d76c09c2c4a986f5f4c624b12b8131
Michael McMillan <email@michaelmcmillan.net>
2015-12-08 23:51:41 +0100

    separate out tests: author and title

---
+++ test/author_test.py
@@ -0,0 +1,25 @@
+import unittest
+from book import Book
+from unittest.mock import MagicMock
+
+class TestBook(unittest.TestCase):
+
+    def test_there_are_no_authors_by_default(self):
+        book = Book()
+        assert len(book.authors) == 0
+
+    def test_single_author_can_be_added(self):
+        book = Book()
+        book.authors = MagicMock('Author')
+        assert len(book.authors) == 1
+
+    def test_multiple_authors_can_be_added_at_once(self):
+        book = Book()
+        book.authors = [MagicMock('Author'), MagicMock('Author')]
+        assert len(book.authors) == 2
+
+    def test_same_author_can_not_be_added_twice(self):
+        with self.assertRaisesRegex(ValueError, 'already'):
+            book = Book()
+            author = MagicMock('Author')
+            book.authors = [author, author]
