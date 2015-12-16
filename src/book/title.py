a4b0a4f4534ac439f56aac7a02cf57c7194a7e58
Michael McMillan <email@michaelmcmillan.net>
2015-12-16 19:39:57 +0100

    rename to max_title_length

--- src/book/title.py
+++ src/book/title.py
@@ -2,7 +2,7 @@ from html import escape
 
 class Title:
 
-    max_length = 256
+    max_title_length = 256
 
     def __init__(self, text=None):
         if self.is_text_valid(text):
@@ -12,14 +12,14 @@ class Title:
         if self.is_empty(text):
             raise ValueError('Title can not be empty.')
         elif self.is_too_long(text):
-            raise ValueError('Title is longer than %d.' % self.max_length)
+            raise ValueError('Title is longer than %d.' % self.max_title_length)
         return True
 
     def is_empty(self, text):
         return not text or text.isspace()
 
     def is_too_long(self, text):
-        return len(text) > self.max_length
+        return len(text) > self.max_title_length
 
     @property
     def text(self):
