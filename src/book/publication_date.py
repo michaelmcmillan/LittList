6e1744147ce94fa7d6ea84ab61ffaf2462f715ee
Michael McMillan <email@michaelmcmillan.net>
2015-12-17 18:14:42 +0100

    re-implemented publication date to not use datetime as we dont really care

--- src/book/publication_date.py
+++ src/book/publication_date.py
@@ -2,22 +2,49 @@ from datetime import datetime
 
 class PublicationDate:
 
-    def __init__(self, date=None):
-        self._date = date if self.is_valid(date) else None
+    def __init__(self, year=None, month=None, day=None):
+        self.year = year \
+            if self.is_valid_year(year) else None
+        self.month = month \
+            if self.is_valid_month(month) else None
+        self.day = day \
+            if self.is_valid_day(day) else None
 
-    @staticmethod
-    def is_valid(date):
-        if date is None:
-            return False
-        elif PublicationDate.is_in_future(date):
-            raise ValueError('Date can not be in the future.')
-        else:
+        if self.year and self.month and self.day:
+            self.is_valid_date(self.year, self.month, self.day)
+
+    def is_valid_day(self, day):
+        if day is None:
             return True
+        elif not self.month:
+            raise ValueError('Day can not be set without month.')
+        elif day < 1 or day > 31:
+            raise ValueError('Day must be between 1 and 31.')
+        return True
 
-    @staticmethod
-    def is_in_future(date):
-        return datetime.now() < date
+    def is_valid_month(self, month):
+        if month is None:
+            return True
+        elif not self.year:
+            raise ValueError('Month can not be set without year.')
+        elif month < 1 or month > 12:
+            raise ValueError('Month must be between 1 and 12.')
+        return True
 
-    @property
-    def date(self):
-        return self._date
+    def is_valid_year(self, year):
+        if year is None:
+            return True
+        if year > self.get_current_year():
+            raise ValueError('Year can not be in the future.')
+        elif year <= 0:
+            raise ValueError('Year must be positive.')
+        return True
+
+    def get_current_year(self):
+        return datetime.today().year
+
+    def is_valid_date(self, year, month, day):
+        try:
+            datetime(year, month, day)
+        except ValueError:
+            raise ValueError('Date is invalid.')
--- test/book_test/publication_date_test.py
+++ test/book_test/publication_date_test.py
@@ -4,19 +4,62 @@ from book.book import Book
 
 class TestPublicationDate(unittest.TestCase):
 
-    def test_is_none_by_default(self):
+    def test_does_not_raise_error_if_year_in_past(self):
         book = Book()
-        assert book.publication_date == None
+        book.publication_date = (2008, None, None)
+        self.assertEqual(book.publication_date.year, 2008)
 
-    def test_does_not_raise_error_if_in_past(self):
-        book = Book()
-        today = datetime.now()
-        tomorrow = today + timedelta(days = -1)
-        book.publication_date = tomorrow
-        assert book.publication_date == tomorrow
+    def test_raise_error_if_year_is_negative(self):
+        with self.assertRaisesRegex(ValueError, 'positive'):
+            book = Book()
+            book.publication_date = (-1, None, None)
 
-    def test_raises_error_if_set_in_the_future(self):
+    def test_raise_error_if_year_is_zero(self):
+        with self.assertRaisesRegex(ValueError, 'positive'):
+            book = Book()
+            book.publication_date = (0, None, None)
+
+    def test_raises_error_if_year_is_in_the_future(self):
         with self.assertRaisesRegex(ValueError, 'future'):
             book = Book()
-            today = datetime.now()
-            book.publication_date = today + timedelta(days = 1)
+            book.publication_date = (2999, None, None)
+
+    def test_does_not_raise_error_if_year_and_month_in_past(self):
+        book = Book()
+        book.publication_date = (2008, 1, None)
+        assert book.publication_date.month == 1
+
+    def test_raise_error_if_month_is_greater_than_12(self):
+        with self.assertRaisesRegex(ValueError, '12'):
+            book = Book()
+            book.publication_date = (2008, 13, None)
+
+    def test_raise_error_if_month_is_smaller_than_1(self):
+        with self.assertRaisesRegex(ValueError, '12'):
+            book = Book()
+            book.publication_date = (2008, 0, None)
+
+    def test_raise_error_if_day_is_smaller_than_1(self):
+        with self.assertRaisesRegex(ValueError, '1'):
+            book = Book()
+            book.publication_date = (2008, 1, 0)
+
+    def test_raise_error_if_day_is_greater_than_31(self):
+        with self.assertRaisesRegex(ValueError, '31'):
+            book = Book()
+            book.publication_date = (2008, 1, 32)
+
+    def test_invalid_date_raises_error(self):
+        with self.assertRaisesRegex(ValueError, 'invalid'):
+            book = Book()
+            book.publication_date = (2008, 2, 31)
+
+    def test_year_must_be_supplied_for_month_to_work(self):
+        with self.assertRaisesRegex(ValueError, 'year'):
+            book = Book()
+            book.publication_date = (None, 1, 1)
+
+    def test_month_must_be_supplied_for_day_to_work(self):
+        with self.assertRaisesRegex(ValueError, 'month'):
+            book = Book()
+            book.publication_date = (2008, None, 1)
