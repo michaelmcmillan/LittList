169a257c1053e6605484e58f73069cc93ed2dbe9
Michael McMillan <email@michaelmcmillan.net>
2015-12-17 18:37:55 +0100

    rearranged class defs to match arg order

--- src/book/publication_date.py
+++ src/book/publication_date.py
@@ -1,5 +1,23 @@
 from datetime import datetime
 
+class Year:
+
+    def __init__(self, year):
+        self.number = year if self.is_valid(year) else None
+
+    def is_valid(self, year):
+        if year is None:
+            return True
+        if year > Year.get_current():
+            raise ValueError('Year can not be in the future.')
+        elif year <= 0:
+            raise ValueError('Year must be positive.')
+        return True
+
+    @staticmethod
+    def get_current():
+        return datetime.today().year
+
 class Month:
 
     def __init__(self, month):
@@ -23,24 +41,6 @@ class Day:
         elif day < 1 or day > 31:
             raise ValueError('Day must be between 1 and 31.')
         return True
-
-class Year:
-
-    def __init__(self, year):
-        self.number = year if self.is_valid(year) else None
-
-    def is_valid(self, year):
-        if year is None:
-            return True
-        if year > Year.get_current():
-            raise ValueError('Year can not be in the future.')
-        elif year <= 0:
-            raise ValueError('Year must be positive.')
-        return True
-
-    @staticmethod
-    def get_current():
-        return datetime.today().year
 
 class PublicationDate:
 
