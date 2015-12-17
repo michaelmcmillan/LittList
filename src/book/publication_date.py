ddaba91aa6ece9b1750ad5b2de70ac419b35260f
Michael McMillan <email@michaelmcmillan.net>
2015-12-17 18:36:42 +0100

    refactored by extracting methods to classes in pubdate: year, month and day

--- src/book/publication_date.py
+++ src/book/publication_date.py
@@ -1,50 +1,67 @@
 from datetime import datetime
 
-class PublicationDate:
+class Month:
 
-    def __init__(self, year=None, month=None, day=None):
-        self.year = year \
-            if self.is_valid_year(year) else None
-        self.month = month \
-            if self.is_valid_month(month) else None
-        self.day = day \
-            if self.is_valid_day(day) else None
+    def __init__(self, month):
+        self.number = month if self.is_valid(month) else None
 
-        if self.year and self.month and self.day:
-            self.is_valid_date(self.year, self.month, self.day)
-
-    def is_valid_day(self, day):
-        if day is None:
-            return True
-        elif not self.month:
-            raise ValueError('Day can not be set without month.')
-        elif day < 1 or day > 31:
-            raise ValueError('Day must be between 1 and 31.')
-        return True
-
-    def is_valid_month(self, month):
+    def is_valid(self, month):
         if month is None:
             return True
-        elif not self.year:
-            raise ValueError('Month can not be set without year.')
         elif month < 1 or month > 12:
             raise ValueError('Month must be between 1 and 12.')
         return True
 
-    def is_valid_year(self, year):
+class Day:
+
+    def __init__(self, day):
+        self.number = day if self.is_valid(day) else None
+
+    def is_valid(self, day):
+        if day is None:
+            return True
+        elif day < 1 or day > 31:
+            raise ValueError('Day must be between 1 and 31.')
+        return True
+
+class Year:
+
+    def __init__(self, year):
+        self.number = year if self.is_valid(year) else None
+
+    def is_valid(self, year):
         if year is None:
             return True
-        if year > self.get_current_year():
+        if year > Year.get_current():
             raise ValueError('Year can not be in the future.')
         elif year <= 0:
             raise ValueError('Year must be positive.')
         return True
 
-    def get_current_year(self):
+    @staticmethod
+    def get_current():
         return datetime.today().year
 
-    def is_valid_date(self, year, month, day):
+class PublicationDate:
+
+    def __init__(self, year=None, month=None, day=None):
+        self.year = Year(year).number
+        self.month = Month(month).number
+        self.day = Day(day).number
+        self.validate(year, month, day)
+
+    def validate(self, year, month, day):
+        if month and not self.year:
+            raise ValueError('Month can not be set without year.')
+        elif day and not self.month:
+            raise ValueError('Day can not be set without month.')
+        elif (self.year and self.month and self.day) \
+        and not self.is_valid_date_context(self.year, self.month, self.day):
+            raise ValueError('Date is invalid.')
+
+    def is_valid_date_context(self, year, month, day):
         try:
             datetime(year, month, day)
+            return True
         except ValueError:
-            raise ValueError('Date is invalid.')
+            return False
