b70841fb35f165dd758059eec44c1db476ebda86
Michael McMillan <email@michaelmcmillan.net>
2015-12-17 19:11:07 +0100

    made book extend a base class called reference

--- src/book/book.py
+++ src/book/book.py
@@ -1,17 +1,13 @@
+from reference.reference import Reference
 from .isbn import ISBN
-from .title import Title
-from .byline import Byline
 from .publisher import Publisher
-from .publication_date import PublicationDate
 
-class Book:
+class Book(Reference):
 
     def __init__(self):
+        Reference.__init__(self)
         self._isbn = ISBN()
-        self._title = Title('Unknown title')
-        self._bylines = Byline()
         self._publisher = Publisher('Unknown publisher')
-        self._publication_date = PublicationDate()
 
     @property
     def isbn(self):
@@ -20,31 +16,6 @@ class Book:
     @isbn.setter
     def isbn(self, number):
         self._isbn = ISBN(number)
-
-    @property
-    def title(self):
-        return self._title.text
-
-    @title.setter
-    def title(self, text):
-        self._title = Title(text)
-
-    @property
-    def authors(self):
-        return self._bylines
-
-    @authors.setter
-    def authors(self, authors):
-        self._bylines.add(authors)
-
-    @property
-    def publication_date(self):
-        return self._publication_date
-
-    @publication_date.setter
-    def publication_date(self, date):
-        year, month, day = date
-        self._publication_date = PublicationDate(year, month, day)
 
     @property
     def publisher(self):
---
+++ src/reference/reference.py
@@ -0,0 +1,36 @@
+from .title import Title
+from .byline import Byline
+from .publication_date import PublicationDate
+
+class Reference:
+
+    def __init__(self):
+        self._title = Title('Unknown title')
+        self._bylines = Byline()
+        self._publication_date = PublicationDate()
+
+    @property
+    def title(self):
+        return self._title.text
+
+    @title.setter
+    def title(self, text):
+        self._title = Title(text)
+
+    @property
+    def authors(self):
+        return self._bylines
+
+    @authors.setter
+    def authors(self, authors):
+        self._bylines.add(authors)
+
+    @property
+    def publication_date(self):
+        return self._publication_date
+
+    @publication_date.setter
+    def publication_date(self, date):
+        year, month, day = date
+        self._publication_date = PublicationDate(year, month, day)
+
