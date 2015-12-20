0077eff9d16ff664bfd2da34c4fdfe8cc977db2a
Michael McMillan <email@michaelmcmillan.net>
2015-12-20 20:25:50 +0100

    remove unused record

--- src/database/records/reference_in_bibliography_record.py
+++
@@ -1,8 +0,0 @@
-from peewee import ForeignKeyField, CharField, IntegerField
-from database.records.base_record import BaseRecord
-from database.records.bibliography_record import BibliographyRecord
-from database.records.reference_record import ReferenceRecord
-
-class ReferenceInBibliographyRecord(BaseRecord):
-    bibliography = ForeignKeyField(BibliographyRecord, related_name='contains')
-    reference = ForeignKeyField(ReferenceRecord, related_name='references')
