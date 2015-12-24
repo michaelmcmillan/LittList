76427411ae7c676d11a9ce5c97292fc886795c48
Michael McMillan <email@michaelmcmillan.net>
2015-12-24 13:23:00 +0100

    improve test for dashes not being in isbn output

--- test/book_test/isbn_test.py
+++ test/book_test/isbn_test.py
@@ -11,7 +11,7 @@ class TestISBN(unittest.TestCase):
     def test_does_not_raise_exception_if_valid(self):
         book = Book()
         book.isbn = '978-1-56619-909-4'
-        assert '978' in book.isbn
+        assert not '-' in book.isbn
 
     def test_dashes_are_removed_from_isbn(self):
         book = Book()
