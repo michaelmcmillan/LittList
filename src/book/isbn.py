75c07b77413616bbe38779da6acac18f6caa7ffc
Michael McMillan <email@michaelmcmillan.net>
2015-12-26 15:03:07 +0100

    let isbn be set to none

--- src/book/isbn.py
+++ src/book/isbn.py
@@ -8,7 +8,7 @@ class ISBN:
 
     def is_valid(self, number):
         if number is None:
-            return False
+            return True
         elif not self.is_properly_formatted(number):
             raise ValueError('Invalid ISBN number.')
         else:
@@ -19,4 +19,5 @@ class ISBN:
 
     @property
     def number(self):
-        return get_canonical_isbn(self._number)
+        return get_canonical_isbn(self._number) \
+            if self._number else None
--- test/book_test/isbn_test.py
+++ test/book_test/isbn_test.py
@@ -12,3 +12,12 @@ class TestISBN(unittest.TestCase):
         book = Book()
         book.isbn = '979-10-90636-07-1'
         assert book.isbn == '9791090636071'
+
+    def test_isbn_can_be_set_to_none(self):
+        book = Book()
+        book.isbn = None
+        assert book.isbn == None
+
+    def test_isbn_is_initially_set_to_none(self):
+        book = Book()
+        assert book.isbn == None
