e9a7c9e99faf86442055c075cb20b325105e8d35
Michael McMillan <email@michaelmcmillan.net>
2015-12-27 14:54:28 +0100

    removed untested lines

--- src/reference/reference.py
+++ src/reference/reference.py
@@ -33,6 +33,3 @@ class Reference:
     def publication_date(self, date):
         year, month, day = date
         self._publication_date = PublicationDate(year, month, day)
-
-    def __hash__(self):
-        return self.id if hasattr(self, 'id') else 0
