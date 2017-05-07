4adefd940f735ffa2cd207977f7f05872d5a6180
Michael McMillan <email@michaelmcmillan.net>
2017-05-07 19:16:22 +0200

    added a library that pings the sources sequentially

---
+++ src/library/library.py
@@ -0,0 +1,21 @@
+from oria import Oria
+from nasjonalbiblioteket import Nasjonalbiblioteket
+
+class Library:
+
+    SOURCES = {
+        'Oria': Oria(),
+        'NB': Nasjonalbiblioteket()
+    }
+
+    def __init__(self, sources=None):
+        self.sources = sources \
+            if sources else self.SOURCES
+
+    def search(self, query):
+        results = []
+        for name, source in self.sources.items():
+            identifiers = source.search(query)
+            results += [(name, result) for result \
+                in source.read_multiple(identifiers)]
+        return results
