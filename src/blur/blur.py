99230048dc0796dffe411d0ce9c3815cb8e1b549
Michael McMillan <email@michaelmcmillan.net>
2017-05-07 19:18:13 +0200

    renamed vars to make class easier to reason about

--- src/blur/blur.py
+++ src/blur/blur.py
@@ -6,17 +6,18 @@ class Blur:
 
     def __init__(self, text):
         self.text = text
-        self.size = (500, 800)
+        self.size = (670, 200)
         self.font = ImageFont.truetype(join(dirname(__file__), 'times.ttf'), 16)
         self.blur = ImageFilter.GaussianBlur(radius=2)
         self.black = (0, 0, 0)
         self.white = (255, 255, 255)
 
     def generate_image(self):
-        image = Image.new('RGBA', self.size, self.white)
-        draw = ImageDraw.Draw(image)
+        canvas = Image.new('RGBA', self.size, self.white)
+        draw = ImageDraw.Draw(canvas)
         draw.text((0, 0), self.text, self.black, font=self.font)
-        image = image.filter(self.blur)
-        raw_image = BytesIO()
-        image.save(raw_image, format='PNG')
-        return raw_image
+        blurred = canvas.filter(self.blur)
+        #blurred.save('lol.png', format='PNG')
+        image_contents = BytesIO()
+        canvas.save(image_contents, format='PNG')
+        return image_contents
