61a501ac563ec9a7cb24d6bbfbec9b83ee422c96
Michael McMillan <email@michaelmcmillan.net>
2017-05-28 14:48:00 +0200

    document methods

--- src/library/web/web.py
+++ src/library/web/web.py
@@ -7,12 +7,14 @@ class Web:
         self.http_client = http_client
 
     def parse(self, url, html):
+        '''Parses HTML using Newspaper.'''
         article = Article(url)
         article.set_html(html)
         article.parse()
         return article.__dict__
 
     def read(self, url):
+        '''Fetches the HTML from the url.'''
         html = self.http_client.get()
         fields = self.parse(url, html)
         return WebConverter.convert(fields)
