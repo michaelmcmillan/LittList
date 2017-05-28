a707a34df95328baf5092c4204cb906ed6aba078
Michael McMillan <email@michaelmcmillan.net>
2017-05-28 16:14:15 +0200

    cache the parsed website

--- src/library/web/web.py
+++ src/library/web/web.py
@@ -1,3 +1,4 @@
+from cache import Cache
 from newspaper import Article
 from http_client import HTTPClient
 from .converter import WebConverter
@@ -5,6 +6,7 @@ from .converter import WebConverter
 class Web:
 
     def __init__(self, http_client=None):
+        self.cache = Cache()
         self.http_client = http_client or HTTPClient()
 
     def parse(self, url, html):
@@ -16,6 +18,7 @@ class Web:
 
     def read(self, url):
         '''Fetches the HTML from the url.'''
-        html = self.http_client.get(url, {})
-        fields = self.parse(url, html)
+        fields = self.cache.get_or_set(url, lambda: 
+            self.parse(url, self.http_client.get(url, {}))
+        )
         return WebConverter.convert(fields)
--- test/unit/library/website/test_website.py
+++ test/unit/library/website/test_website.py
@@ -3,8 +3,12 @@ from fixtures import load_fixture
 from unittest.mock import MagicMock
 from datetime import datetime
 from library import Web, Author
+from cache import Cache
 
 class TestWebsite(TestCase):
+
+    def setUp(self):
+        Cache.flush()
 
     def test_returns_title(self):
         http_client = MagicMock()
@@ -68,3 +72,16 @@ class TestWebsite(TestCase):
         web = Web(http_client)
         website = web.read('http://www.vg.no/a/2408092-no-title')
         self.assertEqual(website.title, None)
+
+class TestWebCache(TestCase):
+
+    def setUp(self):
+        Cache.flush()
+
+    def test_does_not_hit_server_twice_for_same_query(self):
+        http_client = MagicMock()
+        http_client.get.return_value = load_fixture('web/vg-2408092-no-title.html')
+        web = Web(http_client)
+        web.read('http://vg.no/a/2408092')
+        web.read('http://vg.no/a/2408092')
+        self.assertEqual(http_client.get.call_count, 1)
