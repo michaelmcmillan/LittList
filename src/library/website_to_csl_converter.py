693f849090ec0255e7d273f7b24b1e6f6b53f3c8
Michael McMillan <email@michaelmcmillan.net>
2017-05-28 17:52:18 +0200

    falsify any values that dont get converted to csl

--- src/library/website_to_csl_converter.py
+++ src/library/website_to_csl_converter.py
@@ -3,9 +3,9 @@ class WebsiteToCSL:
     @classmethod
     def convert(cls, website):
         csl = {}
-        csl['id'] = website.url
+        csl['id'] = website.url or False
         csl['type'] = 'webpage'
-        csl['title'] = website.title
+        csl['title'] = website.title or False
         csl['author'] = [author.__dict__ for author in website.authors]
         csl['issued'] = {'raw': website.publication_date.strftime('%Y-%m-%d') \
             if website.publication_date else None}
