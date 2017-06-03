d5aea6ab2776cc37004262d1c603f32ad7ca55e9
Michael McMillan <email@michaelmcmillan.net>
2017-06-03 09:10:38 +0200

    make mappers into static methods

--- src/bibliography/repository.py
+++ src/bibliography/repository.py
@@ -3,20 +3,6 @@ from uuid import uuid4
 from os.path import join
 from json import loads, dumps
 
-def from_json_to_bibliography(json):
-    json = loads(json)
-    return Bibliography(
-        set(json['references']),
-        json['previous_bibliography_id']
-    )
-
-def from_bibliography_to_json(bibliography):
-    json = {
-        'references': list(bibliography.references),
-        'previous_bibliography_id': bibliography.previous_bibliography_id
-    }
-    return dumps(json)
-
 class BibliographyRepository:
 
     def __init__(self, root_directory='./data'):
@@ -34,14 +20,14 @@ class BibliographyRepository:
         '''Creates a new bibliography file in the users directory.'''
         bibliography_id = self.get_next_identifier()
         with open(self.get_path_to_bibliography(bibliography_id), 'w') as f:
-            f.write(from_bibliography_to_json(bibliography))
+            f.write(self.from_bibliography_to_json(bibliography))
         return bibliography_id
 
     def read(self, bibliography_id):
         '''Returns an existing bibliography if it exists.'''
         try:
             with open(self.get_path_to_bibliography(bibliography_id), 'r') as f:
-                return from_json_to_bibliography(f.read())
+                return self.from_json_to_bibliography(f.read())
         except IOError:
             return None
 
@@ -60,3 +46,19 @@ class BibliographyRepository:
             return bibliography_id
         bibliography = Bibliography(bibliography.references.difference({identifier}), bibliography_id)
         return self.create(bibliography)
+
+    @staticmethod
+    def from_json_to_bibliography(json):
+        json = loads(json)
+        return Bibliography(
+            set(json['references']),
+            json['previous_bibliography_id']
+        )
+
+    @staticmethod
+    def from_bibliography_to_json(bibliography):
+        json = {
+            'references': list(bibliography.references),
+            'previous_bibliography_id': bibliography.previous_bibliography_id
+        }
+        return dumps(json)
