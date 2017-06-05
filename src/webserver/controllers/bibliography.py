061406566b01d759b6783c73f28826e7104b8773
Michael McMillan <email@michaelmcmillan.net>
2017-06-05 15:57:17 +0200

    forgot to negate blurred flag

--- src/webserver/controllers/bibliography.py
+++ src/webserver/controllers/bibliography.py
@@ -11,7 +11,7 @@ class BibliographyController:
         bibliography_id = session['bibliography_id']
         bibliography = repository.read(bibliography_id)
         rendered_bibliography = generator.render(customer, bibliography_id)
-        contains_blurred = any((entry[0] for entry in rendered_bibliography))
+        contains_blurred = any((not entry[0] for entry in rendered_bibliography))
         return render_template(
             'bibliography.html',
             blurred=contains_blurred,
